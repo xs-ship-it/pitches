fcdcecd4695da0c3513f217b0b569e2373af0172
Felix Nderitu <newtnfelix@gmail.com>
2021-11-11 22:10:58 +0300

    import manager and server

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,16 @@ from config import config_options
 
 
 def create_app(config_name):
-
+    # initializing flask extension
     app = Flask(__name__)
+    
+    # creating app configurations
     app.config.from_object(config_options[config_name])
-
-    from main import main as main_blueprint
+    
+    #registering blueprints
+    from.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
+    
     return app
+    
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,5 @@
+from . import views, errors
 from flask import Blueprint
-main =Blueprint('main,__name__')
-from . import views,errors
+main = Blueprint(__name__,'main') 
+  
+  
